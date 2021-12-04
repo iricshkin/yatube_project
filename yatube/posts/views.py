c5cf414c84de2f16044013249e82cf6214f12934
Ирина Фок <fokira@mail.ru>
2021-12-04 17:00:36 +0300

    Final version

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,34 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Добро пожаловать на главную страницу")
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by("-pub_date")[:10]
+    template = "posts/index.html"
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        "posts": posts,
+    }
+    return render(request, template, context)
 
 
+# View-функция для страницы сообщества:
 def group_posts(request, slug):
-    return HttpResponse(f"Посты в группе {slug}")
+    template = "posts/group_list.html"
+    group = get_object_or_404(Group, slug=slug)
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
+    context = {
+        "group": group,
+        "posts": posts,
+    }
+    return render(request, template, context)
