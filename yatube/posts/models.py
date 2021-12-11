5e4fb37c4e9c57ca9712e01497403c84020d897c
Ирина Фок <fokira@mail.ru>
2021-12-11 14:29:56 +0300

    Final version after review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,24 +5,37 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField()
-    description = models.TextField()
+    title = models.CharField("Наименование", max_length=200)
+    slug = models.SlugField("Адрес", unique=True, null=True)
+    description = models.TextField("Описание")
+
+    class Meta:
+        verbose_name = "группa"
+        verbose_name_plural = "группы"
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField("Текст")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name="Автор",
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
-        related_name="group",
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        verbose_name="группа",
     )
+
+    class Meta:
+        ordering = ("-pub_date",)
+        verbose_name = "пост"
+        verbose_name_plural = "посты"
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 # Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
 
+POST_PER_PAGE = 10
 
-# Create your views here.
-def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by("-pub_date")[:10]
+
+def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.all()[:POST_PER_PAGE]
     template = "posts/index.html"
     # В словаре context отправляем информацию в шаблон
     context = {
@@ -20,13 +18,10 @@ def index(request):
 
 
 # View-функция для страницы сообщества:
-def group_posts(request, slug):
+def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
     template = "posts/group_list.html"
     group = get_object_or_404(Group, slug=slug)
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
+    posts = group.posts.all()[:POST_PER_PAGE]
     context = {
         "group": group,
         "posts": posts,
